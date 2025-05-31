87b0707d36972b4ceee8e8039fbb9de15355715e
Kavya <kavyaagar0@gmail.com>
2025-05-31 16:44:08 -0500

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Kavya Agar on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ To Do ListTests/To_Do_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListTests.swift
+//  To Do ListTests
+//
+//  Created by Kavya Agar on 5/31/25.
+//
+
+import Testing
+@testable import To_Do_List
+
+struct To_Do_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
